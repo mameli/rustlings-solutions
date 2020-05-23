0465a180702cc59be9ad0067d53055d03a5ab13d
mame <mameli93@gmail.com>
2020-05-23 16:09:26 +0200

    v3.0 solutions

---
+++ exercises/test4.rs
@@ -0,0 +1,29 @@
+// test4.rs
+// This test covers the sections:
+// - Modules
+// - Macros
+
+// Write a macro that passes the test! No hints this time, you can do it!
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[macro_export]
+    macro_rules! my_macro {
+        ($s:expr) => {
+            "Hello ".to_string() + &$s
+        };
+    }
+
+    #[test]
+    fn test_my_macro_world() {
+        assert_eq!(my_macro!("world!"), "Hello world!");
+    }
+
+    #[test]
+    fn test_my_macro_goodbye() {
+        assert_eq!(my_macro!("goodbye!"), "Hello goodbye!");
+    }
+}
